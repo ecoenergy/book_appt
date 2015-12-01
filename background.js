8cbc7475e46ea19b01f808074d505f03d928a058
harmonicradius <developer@harmonicradius.com>
2015-12-01 18:45:31 -0500

    Update background.js
    updated to auto-set type as Appointment, and select duration of 1 hour

--- background.js
+++ background.js
@@ -9,16 +9,18 @@ $('#llamada_entrante_contacto_telefono').bind('DOMNodeInserted DOMNodeRemoved DO
 			    if (count ==1) 	{firstName = $(this).html();}
 			    if (count ==2) 	{lastName = $(this).html().toUpperCase();}
 			    if (count ==3) 	{address = $(this).html().toUpperCase();}
-			    if (count ==4) 	{city = $(this).html().toUpperCase();}
-			    if (count ==5) 	{province = $(this).html().toUpperCase();}
-			    if (count ==6) 	{postalCode = $(this).html().toUpperCase();}
+			    if (count ==5) 	{city = $(this).html().toUpperCase();}
+			    if (count ==6) 	{province = $(this).html().toUpperCase();}
+			    if (count ==7) 	{postalCode = $(this).html().toUpperCase();}
 
 			  // console.log(cellText,count,firstName);
 				});
 
 		//console.log(' Name: \t'+firstName+'\n Surname: \t'+lastName+'\n Address: \t'+address+', '+city+', '+province+', '+postalCode+'\n'+'Phone:'+ this.innerHTML);
 
-		url = "https://account.appointment-plus.com/ap/ap_admin_v2/slots_frame_v2.php?wait_id=&emp_type_id=&vp_appt_date=20151121&selected_appt_date=20151121&selected_start_time=660&action=makeappt&selected_e_id=2275&vp_e_id=2275&selected_r_id=&e_id=2275&selected_service_id=&vp_service_id=all&vp_service_id_search=vp_service_id_search&next_open_duration=&next_date=20151122&prev_date=20151120&view=day&selected_customer_id=&search_customer_name="+firstName+"&referred_by=&last_name="+lastName+"&address1="+address+"&city="+city+"&state="+province+"&zip="+postalCode+"&day_phone="+this.innerHTML;
+	phone = this.innerHTML;
+
+var url = "https://account.appointment-plus.com/ap/ap_admin_v2/slots_frame_v2.php?selected_appt_date=20151201&selected_start_time=660&action=makeappt&referred_by=&&first_name="+firstName+"&referred_by=&last_name="+lastName+"&address1="+address+"&city="+city+"&state="+province+"&zip="+postalCode+"&day_phone="+phone+"&selected_service_id=3312&selected_duration=60";
 
 	$("body div").remove(".thelink");
 	$("body").append('<DIV class = "thelink"> <a href="'+url+'" style="color: white" target="_NEW">BOOK APPT</a></DIV>');	
