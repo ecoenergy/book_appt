9cbdc19554e18fc91f89fc044c96c83b8acea65a
harmonicradius <developer@harmonicradius.com>
2015-12-01 20:02:22 -0500

    Update background.js

--- background.js
+++ background.js
@@ -20,7 +20,13 @@ $('#llamada_entrante_contacto_telefono').bind('DOMNodeInserted DOMNodeRemoved DO
 
 	phone = this.innerHTML;
 
-var url = "https://account.appointment-plus.com/ap/ap_admin_v2/slots_frame_v2.php?selected_appt_date=20151201&selected_start_time=660&action=makeappt&referred_by=&&first_name="+firstName+"&referred_by=&last_name="+lastName+"&address1="+address+"&city="+city+"&state="+province+"&zip="+postalCode+"&day_phone="+phone+"&selected_service_id=3312&selected_duration=60";
+	//set date to tomorrow's date
+   var today = moment();
+   var tomorrow = today.add(1,'days');
+   appt_date = moment(tomorrow).format("YYYYMMDD");
+   console.log(appt_date);
+
+var url = "https://account.appointment-plus.com/ap/ap_admin_v2/slots_frame_v2.php?selected_appt_date="+appt_date+"&selected_start_time=660&action=makeappt&referred_by=&&first_name="+firstName+"&referred_by=&last_name="+lastName+"&address1="+address+"&city="+city+"&state="+province+"&zip="+postalCode+"&day_phone="+phone+"&selected_service_id=3312&selected_duration=60";
 
 	$("body div").remove(".thelink");
 	$("body").append('<DIV class = "thelink"> <a href="'+url+'" style="color: white" target="_NEW">BOOK APPT</a></DIV>');	
